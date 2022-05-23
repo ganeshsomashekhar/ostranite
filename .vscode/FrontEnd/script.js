82d9f98d893726597417b7c32102a5f5da22c422
Zachary Renkema <zachary.renkema2005@gmail.com>
2022-05-23 13:16:25 -0500

    smooth scroll

--- .vscode/FrontEnd/script.js
+++ .vscode/FrontEnd/script.js
@@ -5,3 +5,17 @@ window.onload=function() {
     });
 };
 */
+
+//jquery smooth scroll 
+$('.nav').on('click', function(e) {
+    if(this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800
+        );
+    }
+});
